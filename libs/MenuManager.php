3703f1cd41a80bce3cfbcfffb3bd2068464d67fa
ASO1701001 <1701001@st.asojuku.ac.jp>
2019-05-10 16:35:17 +0900

    Commit

--- libs/MenuManager.php
+++ libs/MenuManager.php
@@ -1,11 +1,25 @@
 <?php
 class MenuManager {
+    public static function menuGet() {
 
+    }
 
-menuGet()
-menuPush()
-menuSet()
-menuSelect()
-menuDecision()
-menuLogSet()
+    public static function menuPush() {
+
+    }
+    public static function menuSet() {
+
+    }
+
+    public static function menuSelect() {
+
+    }
+
+    public static function menuDecision() {
+
+    }
+
+    public static function menuLogSet() {
+
+    }
 }
