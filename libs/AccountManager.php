b6fbe4bca16bfba5ecb4f20d010ff3e20c1422dc
ASO1701001 <1701001@st.asojuku.ac.jp>
2019-05-10 11:45:21 +0900

    Commit

--- libs/AccountManager.php
+++ libs/AccountManager.php
@@ -1,6 +1,6 @@
 <?php
 class AccountManager {
-    /**
+    /** ログイン
      * @param string $email
      * @param string $password
      * @return int|null
@@ -21,6 +21,25 @@ class AccountManager {
             }
         }
         return null;
+    }
+
+    /**
+     * @param string $user_id
+     * @return array
+     */
+    public static function getFamilyUsrList(string $user_id) {
+        require_once 'DatabaseManager.php';
+        $db = new DatabaseManager();
+
+        $sql = "SELECT f_user_id, f_email FROM FAMILY_USER_INFO WHERE t_user_id = :user_id";
+        $data = $db->fetchAll($sql, array('user_id' => $user_id));
+        $r_data = array();
+        if (count($data) > 0) {
+            foreach ($data as $row) {
+                $r_data[] = array('user_id' => $row['f_user_id'], 'email' => $row['f_email']);
+            }
+        }
+        return $r_data;
     }
 
 //    /** ワンタイムパスワードを生成する
